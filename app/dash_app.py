d660cc6b85269a4f4d37c1a1d9adf995304467c5
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2024-06-28 00:45:17 +0200

    Create

---
+++ app/dash_app.py
@@ -0,0 +1,36 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+from flask import Flask
+from app import create_app
+import pandas as pd
+import plotly.express as px
+
+server = create_app()
+app = dash.Dash(__name__, server=server, url_base_pathname='/dash/')
+
+# Sample data for initial visualization
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
